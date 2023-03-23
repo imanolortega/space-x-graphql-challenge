092f7b9e5d7301180596ac90e061f4d2a0ac188c
imanol <imanol@easytechgreen.com>
2023-03-23 03:34:51 -0300

    Scroll top on page change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,22 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ApolloProvider } from '@apollo/client'
 import client from '@/utils/apolloClient'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      window.scrollTo({ top: 0, behavior: 'smooth' })
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />
